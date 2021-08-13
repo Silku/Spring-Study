89fad521b09c5936ca8600a74c6d59fb620f8f5e
김태우 <kokicola@naver.com>
2021-08-13 17:53:24 +0900

    21.08.13 스프링을 이용한 로그인 mvc

--- mvcStudy/src/main/java/com/spring/mvc/dao/LoginDAO.java
+++ mvcStudy/src/main/java/com/spring/mvc/dao/LoginDAO.java
@@ -1,12 +1,17 @@
 package com.spring.mvc.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.spring.mvc.model.LoginModel;
+import com.spring.mvc.model.UserBean;
 
 @Repository
 public class LoginDAO {
@@ -17,8 +22,36 @@ public class LoginDAO {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	public int loginCheck(LoginModel loginModel) {
-		String sql = "select count(*) from user_mst where user_id = ? and user_pwd ?";
-		int flag = jdbcTemplate.queryForObject(sql, Integer.class, loginModel.getLogin_id(), loginModel.getLogin_pwd());
+		String sql = "select count(*) from user_mst where user_id = ? and user_pwd = ?";
+		int flag = jdbcTemplate.queryForObject(sql, new Object[] {loginModel.getLogin_id(), loginModel.getLogin_pwd()},
+				new RowMapper<Integer>() {
+			@Override
+			public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return rs.getInt(1);
+					// 무명클래스임
+					/*
+						RowMapper의 동작 자체가 데이터를 불러오면 int rowNum의 값을 받음.
+						컬럼에 값이 있냐 없냐를 확인한다음 rs를 되돌려줌.
+						
+					*/
+					}
+		});
 		return flag;
+	}
+	public UserBean getUserBean(LoginModel loginModel) {
+		String sql = "select user_id, user_pwd, user_name, user_birthday from user_mst where user_id = ? and user_pwd = ?";
+		UserBean userBean = jdbcTemplate.queryForObject(sql, new Object[] {loginModel.getLogin_id(), loginModel.getLogin_pwd()},
+				new RowMapper<UserBean>() {
+			@Override
+			public UserBean mapRow(ResultSet rs, int rowNum) throws SQLException {
+				UserBean bean = new UserBean();
+				bean.setUser_id(rs.getString(1));
+				bean.setUser_pwd(rs.getString(2));
+				bean.setUser_name(rs.getString(3));
+				bean.setUser_birthday(rs.getString(4));
+				return null;
+			}
+		});
+		return userBean;
 	}
 }
